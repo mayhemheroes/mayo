1c70d8c247594fe37db16be7ad4f9c0942a86d91
Hugues Delorme <delorme.hugues@fougue.pro>
2022-05-11 17:06:26 +0200

    CI: add macos build target

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -3,10 +3,10 @@
 # Make the script fails on any command error
 set -e
 
-# Force QMake spec in case of Clang
-if [ "${TRAVIS_COMPILER}" == "clang" ]; then
-    export QMAKESPEC=linux-clang-libc++
-fi
+# # Force QMake spec in case of Clang
+# if [ "${TRAVIS_COMPILER}" == "clang" ]; then
+#     export QMAKESPEC=linux-clang-libc++
+# fi
 
 echo QMAKESPEC=${QMAKESPEC}
 
@@ -16,7 +16,13 @@ cd build
 
 # Run QMake
 qmake --version
-qmake ../mayo.pro CASCADE_INC_DIR=/usr/include/opencascade
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+    qmake ../mayo.pro CASCADE_INC_DIR=/usr/include/opencascade
+fi
+
+if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+    qmake ../mayo.pro CASCADE_INC_DIR=/usr/local/Cellar/opencascade/7.5.3/include/opencascade CASCADE_LIB_DIR=/usr/local/Cellar/opencascade/7.5.3/lib
+fi
 
 # Make
 make -j2
