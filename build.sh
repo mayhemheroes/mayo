becd1eac5118dc8ba4f3a29e5eb79a048392a86a
Raj Shah <rnshah9@asu.edu>
2022-06-05 02:17:36 +0000

    Forgot build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir mayo-build && cd mayo-build
 CASCADE_INC_DIR=`dpkg -L libocct-foundation-dev | grep -i "Standard_Version.hxx" | sed "s/\/Standard_Version.hxx//i"`
 CASCADE_LIB_DIR=`dpkg -L libocct-foundation-dev | grep -i "libTKernel.so" | sed "s/\/libTKernel.so//i"`
-qmake ../mayo CASCADE_INC_DIR=$CASCADE_INC_DIR CASCADE_LIB_DIR=$CASCADE_LIB_DIR
+qmake .. CASCADE_INC_DIR=$CASCADE_INC_DIR CASCADE_LIB_DIR=$CASCADE_LIB_DIR
 make -j8
