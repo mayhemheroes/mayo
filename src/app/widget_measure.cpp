4b598079c184a1e517a4c27e528b6fb4839c6ec8
Hugues Delorme <delorme.hugues@fougue.pro>
2022-06-14 18:44:37 +0200

    WIP1

--- src/app/widget_measure.cpp
+++ src/app/widget_measure.cpp
@@ -9,7 +9,18 @@
 #include "ui_widget_measure.h"
 #include "../gui/gui_document.h"
 
+#include "../graphics/graphics_mesh_object_driver.h"
+#include "../graphics/graphics_shape_object_driver.h"
+
 #include <AIS_Shape.hxx>
+#include <BRepAdaptor_Curve.hxx>
+#include <BRepExtrema_DistShapeShape.hxx>
+#include <BRepGProp.hxx>
+#include <BRep_Tool.hxx>
+#include <GCPnts_AbscissaPoint.hxx>
+#include <GProp_GProps.hxx>
+#include <StdSelect_BRepOwner.hxx>
+#include <TopoDS.hxx>
 
 namespace Mayo {
 
@@ -72,6 +83,165 @@ void WidgetMeasure::onMeasureTypeChanged(int id)
         break;
     }
     } // endswitch
+}
+
+Span<const GraphicsObjectSelectionMode> MeasureShapeDriver::selectionModes(MeasureType type) const
+{
+    switch (type) {
+    case MeasureType::VertexPosition: {
+        static const GraphicsObjectSelectionMode modes[] = { AIS_Shape::SelectionMode(TopAbs_VERTEX) };
+        return modes;
+    }
+    case MeasureType::CircleCenter:
+    case MeasureType::CircleDiameter:
+    case MeasureType::Length:
+    case MeasureType::Angle: {
+        static const GraphicsObjectSelectionMode modes[] = { AIS_Shape::SelectionMode(TopAbs_EDGE) };
+        return modes;
+    }
+    case MeasureType::MinDistance: {
+        static const GraphicsObjectSelectionMode modes[] = {
+            AIS_Shape::SelectionMode(TopAbs_VERTEX),
+            AIS_Shape::SelectionMode(TopAbs_EDGE),
+            AIS_Shape::SelectionMode(TopAbs_FACE)
+        };
+        return modes;
+    }
+    case MeasureType::SurfaceArea: {
+        static const GraphicsObjectSelectionMode modes[] = { AIS_Shape::SelectionMode(TopAbs_FACE) };
+        return modes;
+    }
+    } // endswitch
+
+    return {};
+}
+
+bool MeasureShapeDriver::supports(const GraphicsObjectPtr& object) const
+{
+    auto gfxDriver = GraphicsObjectDriver::get(object);
+    return gfxDriver ? !GraphicsShapeObjectDriverPtr::DownCast(gfxDriver).IsNull() : false;
+}
+
+bool MeasureShapeDriver::supports(MeasureType type) const
+{
+    return type != MeasureType::None;
+}
+
+namespace {
+
+const TopoDS_Shape& getShape(const GraphicsOwnerPtr& owner)
+{
+    static const TopoDS_Shape nullShape;
+    auto brepOwner = Handle_StdSelect_BRepOwner::DownCast(owner);
+    return brepOwner ? brepOwner->Shape() : nullShape;
+}
+
+} // namespace
+
+IMeasureDriver::Result<gp_Pnt> MeasureShapeDriver::vertexPosition(const GraphicsOwnerPtr& owner) const
+{
+    const TopoDS_Shape& shape = getShape(owner);
+    if (shape.IsNull() || shape.ShapeType() != TopAbs_VERTEX)
+        return {};
+
+    return BRep_Tool::Pnt(TopoDS::Vertex(shape));
+}
+
+IMeasureDriver::Result<gp_Pnt> MeasureShapeDriver::circleCenter(const GraphicsOwnerPtr& owner) const
+{
+    const TopoDS_Shape& shape = getShape(owner);
+    if (shape.IsNull() || shape.ShapeType() != TopAbs_EDGE)
+        return {};
+
+    const BRepAdaptor_Curve curve(TopoDS::Edge(shape));
+    return curve.GetType() == GeomAbs_Circle ? curve.Circle().Location() : gp_Pnt{};
+}
+
+IMeasureDriver::Result<QuantityLength> MeasureShapeDriver::circleDiameter(const GraphicsOwnerPtr& owner) const
+{
+    const TopoDS_Shape& shape = getShape(owner);
+    if (shape.IsNull() || shape.ShapeType() != TopAbs_EDGE)
+        return {};
+
+    const BRepAdaptor_Curve curve(TopoDS::Edge(shape));
+    return curve.GetType() == GeomAbs_Circle ? 2 * curve.Circle().Radius() * Quantity_Millimeter : QuantityLength{};
+}
+
+IMeasureDriver::Result<IMeasureDriver::MinDistance> MeasureShapeDriver::minDistance(
+        const GraphicsOwnerPtr& owner1, const GraphicsOwnerPtr& owner2) const
+{
+    const TopoDS_Shape& shape1 = getShape(owner1);
+    if (shape1.IsNull())
+        return {};
+
+    const TopoDS_Shape& shape2 = getShape(owner2);
+    if (shape2.IsNull())
+        return {};
+
+    const BRepExtrema_DistShapeShape dist(shape1, shape2);
+    if (!dist.IsDone())
+        return {};
+
+    IMeasureDriver::MinDistance distResult;
+    distResult.pnt1 = dist.PointOnShape1(1);
+    distResult.pnt2 = dist.PointOnShape2(1);
+    distResult.distance = dist.Value() * Quantity_Millimeter;
+    return distResult;
+}
+
+IMeasureDriver::Result<QuantityLength> MeasureShapeDriver::length(Span<const GraphicsOwnerPtr> spanOwner) const
+{
+    double len = 0;
+    // TODO Parallelize this for-loop. Verify GCPnts_AbscissaPoint::Length() is thread-safe
+    for (const GraphicsOwnerPtr& owner : spanOwner) {
+        const TopoDS_Shape& shape = getShape(owner);
+        if (shape.IsNull() || shape.ShapeType() != TopAbs_EDGE)
+            return {};
+
+        const BRepAdaptor_Curve curve(TopoDS::Edge(shape));
+        len += GCPnts_AbscissaPoint::Length(curve, 1e-6);
+    }
+
+    return len * Quantity_Millimeter;
+}
+
+IMeasureDriver::Result<QuantityAngle> MeasureShapeDriver::angle(
+        const GraphicsOwnerPtr& owner1, const GraphicsOwnerPtr& owner2) const
+{
+    const TopoDS_Shape& shape1 = getShape(owner1);
+    if (shape1.IsNull())
+        return {};
+
+    const TopoDS_Shape& shape2 = getShape(owner2);
+    if (shape2.IsNull())
+        return {};
+
+    const BRepAdaptor_Curve curve1(TopoDS::Edge(shape1));
+    if (curve1.GetType() != GeomAbs_Line)
+        return {};
+
+    const BRepAdaptor_Curve curve2(TopoDS::Edge(shape2));
+    if (curve2.GetType() != GeomAbs_Line)
+        return {};
+
+    return {}; // TODO Implement
+}
+
+IMeasureDriver::Result<QuantityArea> MeasureShapeDriver::surfaceArea(Span<const GraphicsOwnerPtr> spanOwner) const
+{
+    double area = 0;
+    // TODO Parallelize this for-loop. Verify BRepGProp::SurfaceProperties() is thread-safe
+    for (const GraphicsOwnerPtr& owner : spanOwner) {
+        const TopoDS_Shape& shape = getShape(owner);
+        if (shape.IsNull() || shape.ShapeType() != TopAbs_FACE)
+            return {};
+
+        GProp_GProps gprops;
+        BRepGProp::SurfaceProperties(TopoDS::Face(shape), gprops);
+        area += gprops.Mass();
+    }
+
+    return area * Quantity_SquaredMillimeter;
 }
 
 } // namespace Mayo
--- src/app/widget_measure.h
+++ src/app/widget_measure.h
@@ -6,19 +6,75 @@
 
 #pragma once
 
+#include "../base/quantity.h"
+#include "../base/span.h"
 #include "../graphics/graphics_object_ptr.h"
+#include "../graphics/graphics_owner_ptr.h"
 
 #include <QtWidgets/QWidget>
+#include <memory>
+#include <string>
 
 namespace Mayo {
 
 class GuiDocument;
+
+enum class MeasureType {
+    None, VertexPosition, CircleCenter, CircleDiameter, MinDistance, Length, Angle, SurfaceArea
+};
+
+class IMeasureDriver {
+public:
+    virtual Span<const GraphicsObjectSelectionMode> selectionModes(MeasureType type) const = 0;
+    virtual bool supports(const GraphicsObjectPtr& object) const = 0;
+    virtual bool supports(MeasureType type) const = 0;
+
+    template<typename T> struct Result : public T {
+        bool isValid = false;
+        std::string errorMessage;
+        T value;
+        Result() = default;
+        Result(const T& v) : value(v) {}
+        Result(T&& v) : value(std::move(v)) {}
+    };
+
+    struct MinDistance {
+        gp_Pnt pnt1;
+        gp_Pnt pnt2;
+        QuantityLength distance;
+    };
+
+    virtual Result<gp_Pnt> vertexPosition(const GraphicsOwnerPtr& owner) const = 0;
+    virtual Result<gp_Pnt> circleCenter(const GraphicsOwnerPtr& owner) const = 0;
+    virtual Result<QuantityLength> circleDiameter(const GraphicsOwnerPtr& owner) const = 0;
+    virtual Result<MinDistance> minDistance(const GraphicsOwnerPtr& owner1, const GraphicsOwnerPtr& owner2) const = 0;
+    virtual Result<QuantityLength> length(Span<const GraphicsOwnerPtr> spanOwner) const = 0;
+    virtual Result<QuantityAngle> angle(const GraphicsOwnerPtr& owner1, const GraphicsOwnerPtr& owner2) const = 0;
+    virtual Result<QuantityArea> surfaceArea(Span<const GraphicsOwnerPtr> spanOwner) const = 0;
+};
+
+class MeasureShapeDriver : public IMeasureDriver {
+public:
+    Span<const GraphicsObjectSelectionMode> selectionModes(MeasureType type) const override;
+    bool supports(const GraphicsObjectPtr& object) const override;
+    bool supports(MeasureType type) const override;
+
+    Result<gp_Pnt> vertexPosition(const GraphicsOwnerPtr& owner) const override;
+    Result<gp_Pnt> circleCenter(const GraphicsOwnerPtr& owner) const override;
+    Result<QuantityLength> circleDiameter(const GraphicsOwnerPtr& owner) const override;
+    Result<MinDistance> minDistance(const GraphicsOwnerPtr& owner1, const GraphicsOwnerPtr& owner2) const override;
+    Result<QuantityLength> length(Span<const GraphicsOwnerPtr> spanOwner) const override;
+    Result<QuantityAngle> angle(const GraphicsOwnerPtr& owner1, const GraphicsOwnerPtr& owner2) const override;
+    Result<QuantityArea> surfaceArea(Span<const GraphicsOwnerPtr> spanOwner) const override;
+};
 
 class WidgetMeasure : public QWidget {
     Q_OBJECT
 public:
     WidgetMeasure(GuiDocument* guiDoc, QWidget* parent = nullptr);
     ~WidgetMeasure();
+
+    static void addMeasureDriver(std::unique_ptr<IMeasureDriver> driver);
 
 signals:
     void sizeAdjustmentRequested();
